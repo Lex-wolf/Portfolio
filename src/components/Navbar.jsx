1c3f166d01ddca7cca736609656fd6bbf5f4747c
Alejandro Curiel <153023154+Lex-wolf@users.noreply.github.com>
2024-08-09 18:44:51 -0700

    updated logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,14 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex item-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-s w-10" src={logo} alt="logo" />
+        <a
+          href={"https://alexcuriel.com/"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block"
+        >
+          <img className="mx-s w-10" src={logo} alt="logo" />
+        </a>
       </div>
 
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
