087a46fea11bf6509568b3cb91295869174ab820
Alejandro Curiel <153023154+Lex-wolf@users.noreply.github.com>
2025-03-19 21:36:35 -0700

    updated profile pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/perfil.jpg";
+import profilePic from "../assets/pfolio.jpeg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
