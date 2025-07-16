853175ff82e8b6318d7e0a7d8f5a3450d8cdc750
Alejandro Curiel <153023154+Lex-wolf@users.noreply.github.com>
2025-07-16 02:50:23 -0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
         transition={{ duration: 0.5 }}
         className="my-20 text-center text-4xl"
       >
-        Projects
+        Frontend Projects
       </motion.h2>
 
       <div>
