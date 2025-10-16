e0ae59419d3f31af905a0d383029deca5a355228
Alejandro Curiel <153023154+Lex-wolf@users.noreply.github.com>
2025-10-15 22:14:19 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
   const [hasAnimated, setHasAnimated] = useState(false);
 
   return (
-    <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">
+    <div className="border-b border-base-darker pb-4">
+      <h2 className="my-20 text-center text-4xl font-semibold text-accent-cyan">
         About
-        <span className="text-neutral-500"> Me</span>
+        <span className="text-base-light"> Me</span>
       </h2>
       <div className="flex flex-wrap">
         <motion.div
@@ -32,7 +32,7 @@ const About = () => {
           className="w-full lg:w-1/2"
         >
           <div className="flex justify-center lg:justify-start">
-            <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+            <p className="my-2 max-w-xl py-6 text-base-light">{ABOUT_TEXT}</p>
           </div>
         </motion.div>
       </div>
